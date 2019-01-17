ffaa1749537125c3f9c2673e38704c62102e2c52
Senjonnes <odeyemioluwaseun.j@gmail.com>
2019-01-17 15:10:42 +0100

    replaced chosen-select with select2

--- js/main.js
+++ js/main.js
@@ -20,12 +20,30 @@ function printcontent(el) {
 
 }
 
-
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
-  
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+// function addCell(){
+//     var table = document.getElementById("tt");
+//     var row = table.insertRow(table.rows.length);
+//     var cell1 = row.insertCell(0);
+//     var cell2 = row.insertCell(1);
+//     var cell3 = row.insertCell(2);
+//     var cell4 = row.insertCell(3);
+//     var cell5 = row.insertCell(4);
+//     var cell6 = row.insertCell(5);
+//     var cell7 = row.insertCell(6);
+//     var cell8 = row.insertCell(7);
+//     var cell9 = row.insertCell(8);
+//     var cell10 = row.insertCell(9);
+//     var cell11 = row.insertCell(9);
+//     cell1.innerHTML = "seun";
+//     cell2.innerHTML = "Dami";
+//     cell3.innerHTML = "seun";
+//     cell4.innerHTML = "Dami";
+//     cell5.innerHTML = "seun";
+//     cell6.innerHTML = "Dami";
+//     cell7.innerHTML = "seun";
+//     cell8.innerHTML = "Dami";
+//     cell9.innerHTML = "seun";
+//     cell10.innerHTML = "Dami";
+//     cell11.innerHTML = "seun";
+// }
 
